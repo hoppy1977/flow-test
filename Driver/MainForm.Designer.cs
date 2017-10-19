e88452ea9c9391d83fc840ff5e255d39a94b6504
Hopcroft <shane.hopcroft@minfos.com.au>
2017-10-19 17:06:52 +1100

    Added Driver project to test programatic execution of SSIS package.

---
+++ Driver/MainForm.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace Driver
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.executeButton = new System.Windows.Forms.Button();
+			this.closeButton = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// executeButton
+			// 
+			this.executeButton.Location = new System.Drawing.Point(242, 143);
+			this.executeButton.Name = "executeButton";
+			this.executeButton.Size = new System.Drawing.Size(75, 23);
+			this.executeButton.TabIndex = 0;
+			this.executeButton.Text = "Engage!";
+			this.executeButton.UseVisualStyleBackColor = true;
+			this.executeButton.Click += new System.EventHandler(this.executeButton_Click);
+			// 
+			// closeButton
+			// 
+			this.closeButton.Location = new System.Drawing.Point(465, 315);
+			this.closeButton.Name = "closeButton";
+			this.closeButton.Size = new System.Drawing.Size(75, 23);
+			this.closeButton.TabIndex = 1;
+			this.closeButton.Text = "Close";
+			this.closeButton.UseVisualStyleBackColor = true;
+			this.closeButton.Click += new System.EventHandler(this.closeButton_Click);
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(552, 350);
+			this.Controls.Add(this.closeButton);
+			this.Controls.Add(this.executeButton);
+			this.Name = "MainForm";
+			this.Text = "MainForm";
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button executeButton;
+		private System.Windows.Forms.Button closeButton;
+	}
+}
+
---
+++ Driver/MainForm.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Windows.Forms;
+//using Microsoft.SqlServer;
+using Microsoft.SqlServer.Dts.Runtime;
+using Application = Microsoft.SqlServer.Dts.Runtime.Application;
+
+namespace Driver
+{
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			InitializeComponent();
+		}
+
+		private void executeButton_Click(object sender, EventArgs e)
+		{
+			string pkgLocation;
+			Package pkg;
+			Application app;
+			DTSExecResult pkgResults;
+
+			pkgLocation = @"D:\Code\flow-test\flow-test\Package.dtsx";
+
+			app = new Application();
+			pkg = app.LoadPackage(pkgLocation, null);
+
+			//Variables vars;
+			//vars = pkg.Variables;
+			//vars["A_Variable"].Value = "Some value";
+
+			//pkgResults = pkg.Execute(null, vars, null, null, null);
+			pkgResults = pkg.Execute();
+
+			if (pkgResults == DTSExecResult.Success)
+				Console.WriteLine("Package ran successfully");
+			else
+				Console.WriteLine("Package failed");
+		}
+
+		private void closeButton_Click(object sender, EventArgs e)
+		{
+			Close();
+		}
+	}
+}
