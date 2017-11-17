96a760307039d5d4b85708f273ff6e89e525a8d4
Hopcroft <shane.hopcroft@minfos.com.au>
2017-11-17 15:18:47 +1100

    GE Program is now broken down into smaller files.

--- Driver/MainForm.cs
+++ Driver/MainForm.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using log4net;
 using System.IO;
+using System.Linq;
 using System.Windows.Forms;
 using System.Xml;
 using System.Xml.Linq;
@@ -55,20 +56,21 @@ namespace Driver
 
 		private void splitFileButton_Click(object sender, EventArgs e)
 		{
-			var inputFile = @"C:\Users\shane\Desktop\FlowTest\sch-2017-09-01-r1.xml";
+			//var inputFile = @"C:\Users\shane\Desktop\FlowTest\sch-2017-09-01-r1.xml";
+			var inputFile = @"C:\Users\shane.hopcroft\Desktop\FlowTest\sch-2017-09-01-r1.xml";
 			var outputFolder = GetWorkingFolder(inputFile);
 			Directory.CreateDirectory(outputFolder);
 
-			//SplitOutInfoElement(inputFile, outputFolder);
+			SplitOutInfoElement(inputFile, outputFolder);
 			SplitOutProgramElements(inputFile, outputFolder);
-			//SplitOutValuesListElement(inputFile, outputFolder);
-			//SplitOutDrugsListElement(inputFile, outputFolder);
-			//SplitOutOrganisationsListElement(inputFile, outputFolder);
+			SplitOutValuesListElement(inputFile, outputFolder);
+			SplitOutDrugsListElement(inputFile, outputFolder);
+			SplitOutOrganisationsListElement(inputFile, outputFolder);
 
 			MessageBox.Show(@"Done!");
 		}
 
-internal void SplitOutInfoElement(string inputFileName, string outputFolder)
+		internal void SplitOutInfoElement(string inputFileName, string outputFolder)
 		{
 			_logger.Info("Processing info element started");
 
@@ -105,9 +107,43 @@ internal void SplitOutInfoElement(string inputFileName, string outputFolder)
 								var programCode = GetProgramCode(ref element);
 
 								// First of all work pull out all the prescribing rules into 
-								var prescribingRules = ExtractPrescribingRules(ref element);
+								var prescribingRules = ExtractPrescribingRules(ref element)
+									.ToList();
 
-								WriteXmlToFile(Path.Combine(outputFolder, reader.Name + "_" + programCode + ".xml"), element);
+								if (prescribingRules.Count() >= 700)
+								{
+									// There are two many prescribing-rules to write out to a single file
+
+									// Create a version of the program without any prescribing-rule elements
+									var programReader = RemovePrescribingRulesFromProgram(element);
+									var strippedProgramDocument = new XmlDocument();
+									strippedProgramDocument.Load(programReader);
+
+									const int passSize = 700;
+									for (var pass = 0; pass * passSize < prescribingRules.Count; pass++)
+									{
+										var programDocument = (XmlDocument)strippedProgramDocument.CloneNode(true);
+										var root = programDocument.DocumentElement;
+
+										var currentPass = prescribingRules
+											.Skip(pass * passSize)
+											.Take(passSize);
+
+										foreach (var prescribingRule in currentPass)
+										{
+											var newElement = programDocument.CreateElement("prescribing-rule");
+											newElement.InnerXml = prescribingRule.InnerXml;
+											root?.InsertAfter(newElement, root.LastChild);
+										}
+
+										programDocument.Save(Path.Combine(outputFolder, "Program_" + programCode + "_" + (pass + 1) + ".xml"));
+									}
+								}
+								else
+								{
+									// The file is small enought that we can write it out verbatim
+									WriteXmlToFile(Path.Combine(outputFolder, reader.Name + "_" + programCode + ".xml"), element);
+								}
 							} while (reader.ReadToNextSibling("program"));
 						}
 					}
@@ -144,22 +180,60 @@ internal void SplitOutInfoElement(string inputFileName, string outputFolder)
 			return programCode;
 		}
 
-		internal IEnumerable<XmlReader> ExtractPrescribingRules(ref XmlReader programReader)
+		internal IEnumerable<XmlDocument> ExtractPrescribingRules(ref XmlReader programReader)
 		{
 			var xElement = XElement.Load(programReader);
 			programReader.Close();
 			programReader = xElement.CreateReader();
 
-			do
+			var prescribingRuleList = new List<XmlDocument>();
+
+			if (programReader.ReadToFollowing("program"))
 			{
-				programReader.ReadToFollowing("prescribing-rule");
+				if (programReader.ReadToFollowing("prescribing-rule"))
+				{
+					do
+					{
+						var prescribingRule = programReader.ReadSubtree();
 
-				 var x = programReader.ReadSubtree();
+						var prescribingRuleDocument = new XmlDocument();
+						prescribingRuleDocument.Load(prescribingRule);
 
-			} while (programReader.ReadToNextSibling("prescribing-rule"));
-			
-			// TODO:
-			return null;
+						prescribingRuleList.Add(prescribingRuleDocument);
+
+					} while (programReader.ReadToNextSibling("prescribing-rule"));
+				}
+			}
+
+			// and here the reader gets reset to the beginning
+			programReader.Close();
+			programReader = xElement.CreateReader();
+
+			return prescribingRuleList;
+		}
+
+		private XmlReader RemovePrescribingRulesFromProgram(XmlReader programReader)
+		{
+			var stream = new MemoryStream();
+			using (var writer = XmlWriter.Create(stream))
+			{
+				while (programReader.Read())
+				{
+					if (programReader.Name == "prescribing-rule")
+					{
+						programReader.Skip();
+					}
+					else
+					{
+						WriteShallowNode(programReader, writer);
+					}
+				}
+			}
+
+			stream.Seek(0, SeekOrigin.Begin); // Reset stream position to read from the beginning
+
+			var reader = XmlReader.Create(stream);
+			return reader;
 		}
 
 		internal void SplitOutValuesListElement(string inputFileName, string outputFolder)
@@ -224,7 +298,7 @@ internal void SplitOutInfoElement(string inputFileName, string outputFolder)
 
 		internal void WriteXmlToFile(string fileName, XmlReader element)
 		{
-			using (var fileStream = File.OpenWrite(fileName))
+			using (var fileStream = File.Open(fileName, FileMode.Create))
 			{
 				using (var writer = XmlWriter.Create(fileStream))
 				{
@@ -237,6 +311,7 @@ internal void SplitOutInfoElement(string inputFileName, string outputFolder)
 		{
 			//var xmlFileName = @"C:\Users\shane\Desktop\FlowTest\Output\organisations-list.xml";
 			var xmlFileName = @"C:\Users\shane\Desktop\FlowTest\Output\program_CA.xml";
+			//var xmlFileName = @"C:\Users\shane\Desktop\FlowTest\Output\program_GE_1.xml";
 			var targetFileName = Path.ChangeExtension(xmlFileName, "csv");
 
 			var excelApplication = new Excel.Application
@@ -246,11 +321,64 @@ internal void SplitOutInfoElement(string inputFileName, string outputFolder)
 			var excelWorkBook = excelApplication.Workbooks.OpenXML(xmlFileName, Type.Missing, Excel.XlXmlLoadOption.xlXmlLoadOpenXml);
 
 			excelWorkBook.SaveAs(targetFileName, Excel.XlFileFormat.xlCSV, Type.Missing, Type.Missing, Type.Missing, Type.Missing);
-			
+
 			excelWorkBook.Close();
 			excelApplication.Workbooks.Close();
 
 			MessageBox.Show(@"Done!");
+		}
+
+		//This method is useful for streaming transformation with the
+		//XmlReader and the XmlWriter. It pushes through single nodes in the stream
+		static void WriteShallowNode(XmlReader reader, XmlWriter writer)
+		{
+			if (reader == null)
+			{
+				throw new ArgumentNullException(nameof(reader));
+			}
+			if (writer == null)
+			{
+				throw new ArgumentNullException(nameof(writer));
+			}
+
+			switch (reader.NodeType)
+			{
+				case XmlNodeType.Element:
+					writer.WriteStartElement(reader.Prefix, reader.LocalName, reader.NamespaceURI);
+					writer.WriteAttributes(reader, true);
+					if (reader.IsEmptyElement)
+					{
+						writer.WriteEndElement();
+					}
+					break;
+				case XmlNodeType.Text:
+					writer.WriteString(reader.Value);
+					break;
+				case XmlNodeType.Whitespace:
+				case XmlNodeType.SignificantWhitespace:
+					writer.WriteWhitespace(reader.Value);
+					break;
+				case XmlNodeType.CDATA:
+					writer.WriteCData(reader.Value);
+					break;
+				case XmlNodeType.EntityReference:
+					writer.WriteEntityRef(reader.Name);
+					break;
+				case XmlNodeType.XmlDeclaration:
+				case XmlNodeType.ProcessingInstruction:
+					writer.WriteProcessingInstruction(reader.Name, reader.Value);
+					break;
+				case XmlNodeType.DocumentType:
+					// ReSharper disable once AssignNullToNotNullAttribute
+					writer.WriteDocType(reader.Name, reader.GetAttribute("PUBLIC"), reader.GetAttribute("SYSTEM"), reader.Value);
+					break;
+				case XmlNodeType.Comment:
+					writer.WriteComment(reader.Value);
+					break;
+				case XmlNodeType.EndElement:
+					writer.WriteFullEndElement();
+					break;
+			}
 		}
 	}
 }
